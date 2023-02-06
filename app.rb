701fc4f3c8c00df8d64007edcc1ad0917e6cb6d0
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2023-02-06 13:28:33 +0300

    Completed assignment

--- app.rb
+++ app.rb
@@ -5,19 +5,15 @@
 
 # p [1,2,3]
 # puts "Hello World!"
-# print "Pass this test, please."
+# puts "Pass this test, please."
 # p [1,2,3]
 
 # def add
     
 # end
 # # 1 + 1
-# puts add + add
+# puts add + add 
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
